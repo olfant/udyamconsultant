2b4785b876cfda4373be536c98aaf1647efd235d
Gourav Vishwakarma <anitagourav7668@gmail.com>
2026-09-18 07:50:42 +0530

    Changes for images

--- index.php
+++ index.php
@@ -123,7 +123,7 @@
 
         <a href="/udyam-registration-certificate-online.php" style="position: relative; display: block;">
             <img
-                src="/assets/image/home-udyam-sewa.jpg"
+                src="/assets/image/home-udyam-sewa.webp"
                 alt="register your business under new udyam registration online"
                 class="img w-100"
             />
